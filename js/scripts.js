8cb20bb3711f624aa2a8b77207ea0fa71aa67708
Josh Overly and timothea mcguire <timothea.mcguire@gmail.com>
2016-01-13 15:23:24 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  var flavors = ["mint chocolate chip", "chocolate", "chocolate chip cookie dough", "strawberry cheescake"];
+
+
+
+
+  $("button#cream").click(function() {
+    flavors.forEach(function(flavor){
+     alert('I love ' + flavor + '!');
+   });
+  });
+
+});
